22a64de1d6c10f44e48379fa8150228d9b2ed1de
Nguyễn Duy <nguyenducduy02@vccorp.vn>
2023-07-25 16:15:49 +0700

    fix UI

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -1,29 +1,27 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Edit User</title>
-</head>
-<body>
-<h1>Edit User</h1>
-<form method="POST" action="{{ route('users.update', ['id' => $user->id]) }}">
-    @csrf
-    @method('PUT')
-    <label>Name:</label>
-    <input type="text" name="first_name" value="{{ $user->first_name }}" required>
-    <br>
-    <label>Name:</label>
-    <input type="text" name="last_name" value="{{ $user->last_name }}" required>
-    <br>
-    <label>Email:</label>
-    <input type="email" name="email" value="{{ $user->email }}" required>
-    <br>
-    <label for="status">Status:</label>
-    <select name="status" id="status">
-        <option value="{{ $user::STATUS_ACTIVE }}" {{ $user->status === $user::STATUS_ACTIVE ? 'selected' : '' }}>Active</option>
-        <option value="{{ $user::STATUS_SUSPENDED }}" {{ $user->status === $user::STATUS_SUSPENDED ? 'selected' : '' }}>Suspended</option>
-        <option value="{{ $user::STATUS_DELETED }}" {{ $user->status === $user::STATUS_DELETED ? 'selected' : '' }}>Deleted</option>
-    </select>
-    <button type="submit">Update</button>
-</form>
-</body>
-</html>
+@extends('users.layout')
+
+@section('content')
+    <body>
+    <h1>Edit User</h1>
+    <form method="POST" action="{{ route('users.update', ['id' => $user->id]) }}">
+        @csrf
+        @method('PUT')
+        <label>Name:</label>
+        <input type="text" name="first_name" value="{{ $user->first_name }}" required>
+        <br>
+        <label>Name:</label>
+        <input type="text" name="last_name" value="{{ $user->last_name }}" required>
+        <br>
+        <label>Email:</label>
+        <input type="email" name="email" value="{{ $user->email }}" required>
+        <br>
+        <label for="status">Status:</label>
+        <select name="status" id="status">
+            <option value="{{ $user::STATUS_ACTIVE }}" {{ $user->status === $user::STATUS_ACTIVE ? 'selected' : '' }}>Active</option>
+            <option value="{{ $user::STATUS_SUSPENDED }}" {{ $user->status === $user::STATUS_SUSPENDED ? 'selected' : '' }}>Suspended</option>
+            <option value="{{ $user::STATUS_DELETED }}" {{ $user->status === $user::STATUS_DELETED ? 'selected' : '' }}>Deleted</option>
+        </select>
+        <button type="submit">Update</button>
+    </form>
+    </body>
+@endsection
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,34 +1,50 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>User List</title>
-</head>
-<body>
-<h1>User List</h1>
-<ul>
-    @foreach($users as $user)
-        <a href="{{ route('users.show', ['id' => $user->id]) }}">
-            <li>{{ $user->first_name }} {{ $user->last_name }} - {{ $user->email }} -
-                @if($user->status === $user::STATUS_ACTIVE)
-                    Active
-                @elseif($user->status === $user::STATUS_SUSPENDED)
-                    Suspended
-                @elseif($user->status === $user::STATUS_DELETED)
-                    Deleted
-                @endif
-            </li>
-        </a>
-        <form method="POST" action="{{ route('users.softDelete', ['id' => $user->id]) }}">
-            @csrf
-            @method('DELETE')
-            <button type="submit">Soft Delete</button>
-        </form>
-        <form method="POST" action="{{ route('users.destroy', ['id' => $user->id]) }}">
-            @csrf
-            @method('DELETE')
-            <button type="submit">Delete</button>
-        </form>
-    @endforeach
-</ul>
-</body>
-</html>
+@extends('users.layout')
+
+@section('content')
+    <h1>User List</h1>
+    <br>
+    <table class="col-12">
+        <thead>
+        <tr>
+            <th>ID</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Status</th>
+            <th>Detail</th>
+            <th>Soft Delete</th>
+            <th>Delete</th>
+        </tr>
+        </thead>
+        @foreach($users as $user)
+            <tr>
+                <td>{{ $user->id }}</td>
+                <td>{{ $user->first_name }}</td>
+                <td>{{ $user->last_name }}</td>
+                <td>{{ $user->email }}</td>
+                <td>@if($user->status === $user::STATUS_ACTIVE)
+                        Active
+                    @elseif($user->status === $user::STATUS_SUSPENDED)
+                        Suspended
+                    @elseif($user->status === $user::STATUS_DELETED)
+                        Deleted
+                    @endif
+                </td>
+                <td><a href="{{ route('users.show', ['id' => $user->id]) }}"><button class="btn-info">Show</button></a></td>
+                <td><form method="POST" action="{{ route('users.softDelete', ['id' => $user->id]) }}">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn-dark">Soft Delete</button></form>
+                </td>
+                <td><form method="POST" action="{{ route('users.destroy', ['id' => $user->id]) }}">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn-danger">Delete</button>
+                    </form>
+                </td>
+            </tr>
+        @endforeach
+    </table>
+    <br>
+    <a href="{{ route('users.create') }}"><button class="btn-primary">Create User</button></a>
+@endsection
